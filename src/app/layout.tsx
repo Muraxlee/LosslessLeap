b70d8dc5188b84b0a418d1d961ee3c496b1183d6
Murali P <fizzgamerowner@gmail.com>
2025-07-17 15:05:28 +0000

    <meta name="google-site-verification" content="yzbJ4W4Qybpzes6V4Hc9-01gB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="yzbJ4W4Qybpzes6V4Hc9-01gBXTBNkIWbXKQK55qcaw" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
